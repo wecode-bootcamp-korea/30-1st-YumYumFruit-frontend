eec2a99d8313960d33ce2ef64b37f14c2ae9e8fe
hyejinKim <hyejinkim@hyejinKims-MacBook-Air.local>
2022-03-02 02:00:00 +0900

    Fix : ProductList 컴포넌트 삭제

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Nav from '../../components/Nav/Nav';
-import ProductList from '../Main/ProductList/ProductList';
 
 function Main() {
   return (
     <main className="main">
       <Nav />
-      <ProductList />
     </main>
   );
 }
