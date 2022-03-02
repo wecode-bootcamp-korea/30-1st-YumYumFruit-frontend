319ba93b3ee0c9f747f97b7d2a171e8868dbd93f
YOOKIKI <ny90208@naver.com>
2022-03-02 18:15:11 +0900

    [30기 노유정 Add : Login 레이아웃 CSS 중간 완성

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Login.scss';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="main">
+        <div className="titleArea">
+          <h2>
+            <span>MEMBERSHIP</span>
+          </h2>
+        </div>
+        <div className="loginWarp">
+          {/* <form>
+            <input></input>
+            <input></input>
+          </form> */}
+          <div className="login">
+            <h3>MEMBER LOGIN</h3>
+            <div className="loginBox">
+              <p className="id">
+                <span>ID</span>
+                <input id-label="memberId" />
+              </p>
+              <p className="password">
+                <span>PASSWORD</span>
+                <input fw-label="memberPassword" />
+              </p>
+              <div className="securityCheck">
+                <span>보안접속</span>
+                {/* <span className="idSave"></span> */}
+              </div>
+              <div className="loginButton">
+                <span>
+                  <button>LOGIN</button>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
