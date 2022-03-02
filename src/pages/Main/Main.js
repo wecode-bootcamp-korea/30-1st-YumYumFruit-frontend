0d605f87055b9b3fd6cf445cf0397f1d91440b08
hyejinKim <hyejinkim@hyejinKims-MacBook-Air.local>
2022-03-02 18:57:34 +0900

    WIP

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Nav from '../../components/Nav/Nav';
 import ProductList from '../Main/ProductList/ProductList';
 
 function Main() {
   return (
     <main className="main">
-      <Nav />
       <ProductList />
     </main>
   );
