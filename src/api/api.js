6c6e40836f8e777573e1953d1e0e7a84dd787664
hyejinKim <hyejinkim@hyejinKims-MacBook-Air.local>
2022-03-09 14:26:55 +0900

    WIP

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 export async function getProducts(query) {
-  const response = await fetch(`http://10.58.2.143:8000/products${query}`);
+  const response = await fetch(`http://10.58.6.197:8000/products${query}`);
   const body = await response.json();
   return body;
 }
