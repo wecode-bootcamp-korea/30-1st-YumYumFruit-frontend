3b51975a7a20655c73cd99617e5101964b8175bf
hyejinKim <hyejinkim@hyejinKims-MacBook-Air.local>
2022-03-10 08:22:51 +0900

    WIP

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,5 @@
 export async function getProducts(query) {
-  // const response = await fetch(`http://10.58.6.197:8000/products${query}`);
-  const response = await fetch(`/data/productListdata.JSON`);
+  const response = await fetch(`http://10.58.6.197:8000/products${query}`);
   const body = await response.json();
   return body;
 }
