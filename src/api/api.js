66b3a838f2b94be45c2fb2722c50438d7dcf126c
hyejinKim <hyejinkim@hyejinKims-MacBook-Air.local>
2022-03-10 16:52:22 +0900

    WIP

--- src/api/api.js
+++ src/api/api.js
@@ -10,6 +10,7 @@ export async function getProducts(query) {
   return body;
 }
 
+// TODO) cart_info만 받고, user_info는 따로 받는다 -> getUserInfo 함수 필요
 export async function getCartList() {
   const response = await fetch(`${BASE_URL}/users/shoppingcart`, {
     headers: {
@@ -42,6 +43,7 @@ export async function deleteProduct(cart_id) {
   });
 }
 
+// TODO) cart_id=1&cart_id=2 ... 형식으로 수정, 마지막은 &를 붙이지 않는다
 export async function deleteCheckedItems(checkedItems) {
   await fetch(`${BASE_URL}/users/shoppingcart?cart_id=${checkedItems}`, {
     method: 'DELETE',
@@ -52,7 +54,7 @@ export async function deleteCheckedItems(checkedItems) {
 }
 
 export async function deleteAllItems() {
-  await fetch(`${BASE_URL}/users/shoppingcart?cart_id=0`, {
+  await fetch(`${BASE_URL}/users/shoppingcart?cart_id=all`, {
     method: 'DELETE',
     headers: {
       Authorization: getToken(),
