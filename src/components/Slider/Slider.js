4bffa3e18f8c03552df066fec98772a1eb303d06
hyejinKim <hyejinkim@hyejinKims-MacBook-Air.local>
2022-03-10 14:59:40 +0900

    Fix : 슬라이드 transition -> scss로 이동

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -9,20 +9,20 @@ function Slider() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const ref = useRef(null);
 
-  const nextSlide = () => {
+  const nextSlideClick = () => {
     currentSlide >= TOTAL_SLIDES
       ? setCurrentSlide(0)
       : setCurrentSlide(currentSlide + 1);
   };
 
-  const prevSlide = () => {
+  const prevSlideClick = () => {
     currentSlide === 0
       ? setCurrentSlide(TOTAL_SLIDES)
       : setCurrentSlide(currentSlide - 1);
   };
 
+  // TODO) 일정 시간 이후 자동으로 슬라이드 전환
   useEffect(() => {
-    ref.current.style.transition = 'all 0.5 ease-in-out';
     ref.current.style.transform = `translateX(-${currentSlide}00%)`;
   }, [currentSlide]);
 
@@ -33,10 +33,10 @@ function Slider() {
           <Slide key={item.id} item={item} />
         ))}
       </ul>
-      <button className="leftBtn" onClick={prevSlide}>
+      <button className="leftBtn" onClick={prevSlideClick}>
         <span className="arrow">&lang;</span>
       </button>
-      <button className="rightBtn" onClick={nextSlide}>
+      <button className="rightBtn" onClick={nextSlideClick}>
         <span className="arrow">&rang;</span>
       </button>
     </section>
