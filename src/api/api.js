584c16c854654d5338c0c4d12d6e3c4fe05bdca8
hyejinKim <hyejinkim@hyejinKims-MacBook-Air.local>
2022-03-10 11:16:46 +0900

    Add : BASE_URL 추가

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,8 @@
+const BASE_URL = 'http://10.58.0.144:8000';
+
+// 상품 목록
 export async function getProducts(query) {
-  const response = await fetch(`http://10.58.6.197:8000/products${query}`);
+  const response = await fetch(`${BASE_URL}/products${query}`);
   const body = await response.json();
   return body;
 }
